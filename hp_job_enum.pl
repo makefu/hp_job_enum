fa8c33572b9666cfd63aaf4ec31915e9af0fe00d
Felix Richter <Felix.Richter@alcatel-lucent.com>
2012-07-27 15:00:29 +0200

    add date/domain/Tool fields
    check for more fields than before to provide more detailed information about the jobs saved

--- hp_job_enum.pl
+++ hp_job_enum.pl
@@ -1,25 +1,43 @@
 #!/usr/bin/perl
 ###################################
 #
-# HP LaserJet SNMP User name enumeration tool v0.2 by
-# Pinion Labs 050705
-# george[46]hedfors[64]pinion[46]se
-# http://www.pinion.se
+# HP LaserJet SNMP User name enumeration tool v0.3 by
+# Author: 
+#         Pinion Labs 050705
+#         george[46]hedfors[64]pinion[46]se
+#         http://www.pinion.se
+#
+# Refinement:
+#	    makefu@Krebsco
+#         makefu &#64 euer.krebsco.de
+#         http://euer.krebsco.de
 #
 # Description
 # HP LaserJet printers loggs recent printed documents with
 # timestamp, size, number of pages, username and machine name.
 # These can be extracted using a specially crafted SNMP Object ID.
 #
-# Document name under 1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.1
-# Document pages under 1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.12
-# Document size under 1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.14
-# Usernames are found under 1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.1
-# Machine names under 1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.2
+# Document name             1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.1
+# Document pages       		1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.12
+# Document size       		1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.14
+# Usernames                 1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.1
+# Machine names       		1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.2
 #
-# Output format
-# DocID:Username:Machine:Pages:Size:DocName
+# Domain                    1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.3
+# Date                      1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.4
+# Print Tool                1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.6
+# Print File                1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.7
+# User Name(again?)         1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.8
 #
+# Old Output format, comment in if you wish
+#  DocID:Username:Machine:Pages:Size:DocName 
+# 
+# Manager Style Output Format
+#  File:
+#  Date:
+#  User:
+#  Tool:
+# 
 #
 
 use Net::SNMP;
@@ -58,27 +76,51 @@ for($i = $startid; $err < $tolerance; $i++) {
  } else {
   $found++;
 
-  ($null, $user) = split(/\=/, $result->{$oid}, 2);
+     ($null, $user) = split(/\=/, $result->{$oid}, 2);
+ 
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.2.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    ($null, $id) = split(/\=/, $result->{$oid}, 2);
 
-  $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.2.$i.0";
-  $result = $session->get_request(-varbindlist => [$oid]);
-  ($null, $id) = split(/\=/, $result->{$oid}, 2);
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.3.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    ($null, $domain) = split(/\=/, $result->{$oid}, 2);
 
-  $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.1.$i.0";
-  $result = $session->get_request(-varbindlist => [$oid]);
-  $doc = hex2ascii($result->{$oid});
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.4.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    ($null, $time) = split(/\=/, $result->{$oid}, 2);
+    if ( $time ){
+       $parsed_time = Time::Piece->strptime($time,"%Y%m%d%H%M%S");
+    }
 
-  $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.12.$i.0";
-  $result = $session->get_request(-varbindlist => [$oid]);
-  $pages = $result->{$oid};
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.6.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    ($null, $tool) = split(/\=/, $result->{$oid}, 2);
 
-  $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.14.$i.0";
-  $result = $session->get_request(-varbindlist => [$oid]);
-  $size = $result->{$oid};
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.7.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    ($null, $tool_exe) = split(/\=/, $result->{$oid}, 2);
 
-  printf("%d:%s:%s:%s:%s:%s\n", $i, $user, $id, $pages, $size, $doc);
-
-  $err = 0;
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.23.8.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    ($null, $also_user) = split(/\=/, $result->{$oid}, 2);
+ 
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.1.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    $doc = hex2ascii($result->{$oid});
+ 
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.12.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    $pages = $result->{$oid};
+ 
+    $oid = "1.3.6.1.4.1.11.2.3.9.4.2.1.1.6.5.14.$i.0";
+    $result = $session->get_request(-varbindlist => [$oid]);
+    $size = $result->{$oid};
+    #printf("%d:%s:%s:%s:%s:%s\n", $i, $user, $id, $pages, $size, $doc);
+    printf("File: %s with %s pages\n",$doc,$pages);
+    printf("Date: %s\n",$parsed_time->strftime("%d.%b.%Y %H:%M:%S"));
+    printf("User: %s\\%s from %s\n",$domain,$user,$id);
+    printf("Tool: %s (%s)\n\n",$tool,$tool_exe);
  }
 }
 
